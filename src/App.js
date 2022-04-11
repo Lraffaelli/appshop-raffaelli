016764a4d32f672e686efbaea69e710d6660e74e
Lraffaelli <lucasraffaelli@outlook.com>
2022-04-11 20:28:10 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App Shop Lucas Raffaelli</h1>
+    </div>
+  );
+}
+
+export default App;
